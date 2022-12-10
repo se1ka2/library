cd4c5ccfd446cf91e2eae439635b835da05b401a
se1ka2 <happinaaru@gmail.com>
2022-12-10 20:55:39 +0900

    add combination

---
+++ lib/math/combination.hpp
@@ -0,0 +1,46 @@
+mint modpow(mint x, long long n)
+{
+    mint res = 1, r = x;
+    while (n)
+    {
+        if (n & 1)
+        {
+            res *= r;
+        }
+        r *= r;
+        n >>= 1;
+    }
+    return res;
+}
+
+struct combination
+{
+    int maxn;
+    std::vector<mint> fact;
+    std::vector<mint> ifact;
+
+    combination(int maxn) : maxn(maxn)
+    {
+        fact.resize(maxn + 1);
+        ifact.resize(maxn + 1);
+        fact[0] = 1;
+        for (int i = 1; i <= maxn; i++)
+        {
+            fact[i] = fact[i - 1] * i;
+        }
+        ifact[maxn] = modpow(fact[maxn], MOD - 2);
+        for (int i = maxn - 1; i >= 0; i--)
+        {
+            ifact[i] = ifact[i + 1] * (i + 1);
+        }
+    }
+
+    mint operator()(int n, int k)
+    {
+        if (k > n || k < 0)
+        {
+            return 0;
+        }
+        return fact[n] * ifact[k] * ifact[n - k];
+    }
+};
---
+++ test/yukicoder/no_2141.test.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://yukicoder.me/problems/no/2156"
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MOD = 998244353;
+
+#include "../../lib/math/modint.hpp"
+#include "../../lib/math/combination.hpp"
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    combination com(m);
+    int s = m;
+    mint ans = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (i < m % n)
+        {
+            ans *= com(s, m / n + 1);
+            s -= m / n + 1;
+        }
+        else
+        {
+            ans *= com(s, m / n);
+            s -= m / n;
+        }
+    }
+    cout << ans << endl;
+}
