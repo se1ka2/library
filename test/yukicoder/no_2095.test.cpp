d6378f6a1460859c04a80b9031ac24abfcf3e745
se1ka2 <happinaaru@gmail.com>
2022-12-12 06:52:53 +0900

    add test yuki 2095

---
+++ test/yukicoder/no_2095.test.cpp
@@ -0,0 +1,43 @@
+#define PROBLEM "https://yukicoder.me/problems/no/2095"
+
+#include <iostream>
+#include <cassert>
+#include <queue>
+using namespace std;
+typedef long long ll;
+
+#include "../../lib/graph/graph.hpp"
+#include "../../lib/graph/shortest_path/dijkstra.hpp"
+
+int a[1005][1005];
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    graph<ll> g(n * m + n);
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            g.add_directed_edge(i * m + j, i + n * m, 0);
+        }
+    }
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            g.add_directed_edge(i + n * m, (i + 1) * m + j, a[i][j] + a[i + 1][j]);
+            g.add_directed_edge(i * m + j, (i + 1) * m + j, a[i + 1][j]);
+        }
+    }
+    vector<ll> d = dijkstra(g, n * m);
+    cout << d[n * m + n - 1] << endl;
+}
