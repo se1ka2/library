4e351d5e3c768e82046ea1c3f15d478c529b3a51
se1ka2 <happinaaru@gmail.com>
2022-12-13 19:22:40 +0900

    add prime_structure and add test yuki 2081

---
+++ lib/math/prime_structure.hpp
@@ -0,0 +1,49 @@
+struct prime_sturcture
+{
+    int max_n;
+    std::vector<bool> p;
+    std::vector<int> prime_list;
+
+    prime_sturcture(int max_n) : max_n(max_n), p(max_n + 1, true)
+    {
+        p[0] = p[1] = false;
+        for (int i = 2; i <= max_n; i++)
+        {
+            if (p[i])
+            {
+                prime_list.push_back(i);
+                for (int j = i * 2; j <= max_n; j += i)
+                {
+                    p[j] = false;
+                }
+            }
+        }
+    }
+
+    bool is_prime(int n)
+    {
+        assert(n <= max_n);
+        return p[n];
+    }
+
+    int size()
+    {
+        return prime_list.size();
+    }
+
+    const int operator[](int i) const
+    {
+        assert(i < (int)prime_list.size());
+        return prime_list[i];
+    }
+
+    std::vector<int>::const_iterator begin()
+    {
+        return prime_list.begin();
+    }
+
+    std::vector<int>::const_iterator end()
+    {
+        return prime_list.end();
+    }
+};
