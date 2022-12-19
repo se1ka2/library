eebadb852e2b8b7da580f13a325355d381c7a352
se1ka2 <happinaaru@gmail.com>
2022-12-19 23:20:44 +0900

    modify low_link

--- lib/graph/other/low_link.hpp
+++ lib/graph/other/low_link.hpp
@@ -1,41 +1,59 @@
 template <typename G, typename T = int>
-void low_link_sub(int u, int last_edge_id, int &k, G &g, std::vector<int> &ord, std::vector<int> &low, std::vector<edge<T>> &bridge)
+struct low_link
 {
-    ord[u] = k++;
-    low[u] = ord[u];
-    for (edge<T> e : g[u])
-    {
-        int v = e.to;
-        if (e.id == last_edge_id)
-        {
-            continue;
-        }
-        if (ord[v] == -1)
-        {
-            low_link_sub(v, e.id, k, g, ord, low, bridge);
-        }
-        low[u] = std::min(low[u], low[v]);
-        if (ord[u] < low[v])
-        {
-            bridge.push_back(e);
-        }
-    }
-}
-
-template <typename G, typename T = int>
-std::vector<edge<T>> low_link(G &g)
-{
-    int n = g.size();
-    std::vector<int> ord(n, -1);
-    std::vector<int> low(n, -1);
+public:
+    std::vector<int> articulation;
     std::vector<edge<T>> bridge;
-    int k = 0;
-    for (int u = 0; u < n; u++)
+
+    low_link(G &g) : n(g.size()), g(g), ord(g.size(), -1), low(g.size(), -1)
     {
-        if (ord[u] == -1)
+        int k = 0;
+        for (int u = 0; u < n; u++)
         {
-            low_link_sub(u, -1, k, g, ord, low, bridge);
+            if (ord[u] == -1)
+            {
+                dfs(u, -1, k);
+            }
         }
     }
-    return bridge;
-}
+
+private:
+    int n;
+    const G &g;
+    std::vector<int> ord;
+    std::vector<int> low;
+
+    void dfs(int u, int last_edge_id, int &k)
+    {
+        low[u] = ord[u] = k++;
+        int c = 0;
+        bool is_artification = false;
+        for (edge<T> e : g[u])
+        {
+            int v = e.to;
+            if (e.id == last_edge_id)
+            {
+                continue;
+            }
+            if (ord[v] == -1)
+            {
+                c++;
+                dfs(v, e.id, k);
+                if (last_edge_id != -1 && ord[u] <= low[v])
+                {
+                    is_artification = true;
+                }
+                if (ord[u] < low[v])
+                {
+                    bridge.push_back(e);
+                }
+            }
+            low[u] = std::min(low[u], low[v]);
+        }
+        is_artification |= (last_edge_id == -1 && c >= 2);
+        if (is_artification)
+        {
+            articulation.push_back(u);
+        }
+    }
+};
