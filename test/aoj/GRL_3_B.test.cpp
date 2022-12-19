053e9412adc21d8179665cd6670d0f75d9d5bde7
se1ka2 <happinaaru@gmail.com>
2022-12-19 23:20:15 +0900

    modify low_link and add test aoj GRL_3_B

---
+++ test/aoj/GRL_3_B.test.cpp
@@ -0,0 +1,35 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_B"
+
+#include <algorithm>
+#include <iostream>
+#include <cassert>
+#include <vector>
+using namespace std;
+typedef pair<int, int> P;
+
+#include "../../lib/graph/graph.hpp"
+#include "../../lib/graph/other/low_link.hpp"
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    graph g(n);
+    for (int i = 0; i < m; i++)
+    {
+        int s, t;
+        cin >> s >> t;
+        g.add_undirected_edge(s, t);
+    }
+    low_link lowlink(g);
+    vector<P> ans;
+    for (edge<int> e : lowlink.bridge)
+    {
+        ans.push_back(P(min(e.from, e.to), max(e.from, e.to)));
+    }
+    sort(ans.begin(), ans.end());
+    for (P p : ans)
+    {
+        cout << p.first << " " << p.second << endl;
+    }
+}
