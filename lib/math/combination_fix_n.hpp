a81e9f56920d376eabd5ba3ef1f0ca289acd8a05
se1ka2 <happinaaru@gmail.com>
2023-01-14 04:07:09 +0900

    modify combination_fix_n

--- lib/math/combination_fix_n.hpp
+++ lib/math/combination_fix_n.hpp
@@ -1,18 +1,4 @@
-mint modpow(mint x, long long n)
-{
-    mint res = 1, r = x;
-    while (n)
-    {
-        if (n & 1)
-        {
-            res *= r;
-        }
-        r *= r;
-        n >>= 1;
-    }
-    return res;
-}
-
+template <typename M>
 struct combination_fix_n
 {
 public:
@@ -28,14 +14,14 @@ public:
             nP[i] = nP[i - 1] * (n + 1 - i);
             fact[i] = fact[i - 1] * i;
         }
-        ifact[maxk] = modpow(fact[maxk], MOD - 2);
+        ifact[maxk] = fact[maxk].inv();
         for (int i = maxk - 1; i >= 0; i--)
         {
             ifact[i] = ifact[i + 1] * (i + 1);
         }
     }
 
-    mint operator()(int k)
+    M operator()(int k)
     {
         assert(k <= maxk);
         if (k > n || k < 0)
@@ -48,7 +34,7 @@ public:
 private:
     long long n;
     int maxk;
-    std::vector<mint> nP;
-    std::vector<mint> fact;
-    std::vector<mint> ifact;
+    std::vector<M> nP;
+    std::vector<M> fact;
+    std::vector<M> ifact;
 };
