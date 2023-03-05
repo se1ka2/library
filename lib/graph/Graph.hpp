465d8d87dd4b7bcd9731af4df654be8602c80e1f
se1ka2 <happinaaru@gmail.com>
2023-03-06 00:28:02 +0900

    modify graph

---
+++ lib/graph/Graph.hpp
@@ -0,0 +1,59 @@
+template <typename T = int>
+struct Graph
+{
+public:
+    struct Edge
+    {
+        int from, to;
+        T cost;
+        int id;
+
+        operator int() const
+        {
+            return to;
+        }
+    };
+
+    Graph() {}
+
+    Graph(int n) : n(n), m(0), g(n) {}
+
+    void add_directed_edge(int from, int to, T cost = 1)
+    {
+        assert(0 <= from && from < n);
+        assert(0 <= to && to < n);
+        g[from].push_back((Edge){from, to, cost, m++});
+    }
+
+    void add_undirected_edge(int from, int to, T cost = 1)
+    {
+        assert(0 <= from && from < n);
+        assert(0 <= to && to < n);
+        g[from].push_back((Edge){from, to, cost, m});
+        g[to].push_back((Edge){to, from, cost, m++});
+    }
+
+    int size()
+    {
+        return n;
+    }
+
+    int edge_size()
+    {
+        return m;
+    }
+
+    inline const std::vector<Edge> &operator[](const int &u) const
+    {
+        return g[u];
+    }
+
+    inline std::vector<Edge> &operator[](const int &u)
+    {
+        return g[u];
+    }
+
+private:
+    int n, m;
+    std::vector<std::vector<Edge>> g;
+};
