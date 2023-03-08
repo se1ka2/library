86c96117a14c95f5444a4c3d43da8c3b30c83f4b
se1ka2 <happinaaru@gmail.com>
2023-03-08 13:24:28 +0900

    modify eularian trail

--- lib/graph/other/eularian_trail.hpp
+++ lib/graph/other/eularian_trail.hpp
@@ -1,43 +1,43 @@
-template <typename G, typename T = int>
-void eulerian_trail_sub(int u, int last_edge_id, bool return_edge_id, G &g, std::vector<int> &trail, std::vector<bool> &used)
+template <typename GRAPH>
+std::vector<int> eulerian_trail(GRAPH &g, int r = 0, bool return_edges = true)
 {
-    for (edge<T> e : g[u])
-    {
-        if (used[e.id])
-        {
-            continue;
-        }
-        used[e.id] = true;
-        eulerian_trail_sub(e.to, e.id, return_edge_id, g, trail, used);
-    }
-    if (!return_edge_id)
-    {
-        trail.push_back(u);
-    }
-    else if (last_edge_id != -1)
-    {
-        trail.push_back(last_edge_id);
-    }
-}
+    int n = g.size();
+    int m = g.edge_size();
+    std::vector<int> trail;
+    std::vector<bool> used(m);
 
-template <typename G>
-std::vector<int> eulerian_trail(G &g, int r = 0, bool return_edge_id = true)
-{
-    int n = g.size(), m = g.edge_size();
+    auto dfs = [&](auto self, int u, int last_edge_id) -> void
+    {
+        for (auto e : g[u])
+        {
+            if (!used[e.id])
+            {
+                used[e.id] = true;
+                self(self, e.to, e.id);
+            }
+        }
+        if (!return_edges)
+        {
+            trail.push_back(u);
+        }
+        else if (last_edge_id != -1)
+        {
+            trail.push_back(last_edge_id);
+        }
+    };
+
     bool even_degree = true;
     for (int u = 0; u < n; u++)
     {
         even_degree &= !((int)g[u].size() % 2);
     }
     assert(even_degree);
-    std::vector<int> trail;
-    std::vector<bool> used(m);
-    eulerian_trail_sub(r, -1, return_edge_id, g, trail, used);
-    bool cover_all_edge = true;
+    dfs(dfs, r, -1);
+    bool connected = true;
     for (int i = 0; i < m; i++)
     {
-        cover_all_edge &= used[i];
+        connected &= used[i];
     }
-    assert(cover_all_edge);
+    assert(connected);
     return trail;
 }
